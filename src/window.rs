f89c1d651262717a4a4f5e36c25d5c03d4d7ac2d
Milan Stastny <milan@acheta.games>
2022-04-10 16:11:06 +0200

    Switch button icon when theme changes

--- src/window.rs
+++ src/window.rs
@@ -144,17 +144,16 @@ pub(crate) mod imp {
             let button = self.color_scheme_btn.get();
             let style_manager = adw::StyleManager::default();
 
-            style_manager.connect_color_scheme_notify(move |style_manager| {
-                let supported = style_manager.system_supports_color_schemes();
-                button.set_visible(!supported);
-                if supported {
+            if !style_manager.system_supports_color_schemes() {
+                style_manager.connect_color_scheme_notify(move |style_manager| {
+                    button.set_visible(true);
                     if style_manager.is_dark() {
                         button.set_icon_name("light-mode-symbolic");
                     } else {
                         button.set_icon_name("dark-mode-symbolic");
                     }
-                }
-            });
+                });
+            }
 
             // load latest window state
             obj.load_window_size();
